00e3da53a1749c47cc295115c5e0ac3b2dd3a8f0
PedroHIB <pedrohizib@gmail.com>
2020-05-04 20:10:52 -0300

    Configurando o botao filtrar

--- js.js
+++ js.js
@@ -113,10 +113,9 @@ if (verificadorOrdem == 1) {
 
 
 //Menu Responsivo
-let show = true; 
+var show = true; 
 const menuSection = document.querySelector(".menu-section")
 const menuToggle = menuSection.querySelector(".menu-toggle")
-
 menuToggle.addEventListener("click", () => {
     document.body.style.overflow = show ? "hidden" : "initial"
 
@@ -124,13 +123,11 @@ menuToggle.addEventListener("click", () => {
     show = !show;
 })
 
-let show2 = true
+var show2 = true
 const menuSection2 = document.querySelector(".menu-section2")
 const menuToggle2 = menuSection2.querySelector(".menu-toggle2")
-
 menuToggle2.addEventListener("click", () => {
     document.body.style.overflow = show2 ? "hidden" : "initial"
-
     menuSection2.classList.toggle("on", show2)
     show2 = !show2;
 })
@@ -138,32 +135,80 @@ menuToggle2.addEventListener("click", () => {
 
 const texto = document.querySelector('.menu-toggle')
 var verificar = true
-
 texto.addEventListener('click', function(){
     if (verificar){
-        document.querySelector('.menu-toggle').innerHTML = "x"
-        document.querySelector('.menu-toggle').style.border = "none"
+        texto.innerHTML = "x"
+        texto.style.border = "none"
         verificar = !verificar
     } else {
-        document.querySelector('.menu-toggle').innerHTML = "Ordenar"
-        document.querySelector('.menu-toggle').style.border = "1px solid"
+        texto.innerHTML = "Ordenar"
+        texto.style.border = "1px solid"
         verificar = true
     }
 })
- 
+
 const texto2 = document.querySelector('.menu-toggle2')
 var verificar = true
-
 texto2.addEventListener('click', function(){
     if (verificar){
-        var bordar = document.querySelector('.menu-toggle2').style.border
-        document.querySelector('.menu-toggle2').innerHTML = "x"
-        document.querySelector('.menu-toggle2').style.border = "none"
-
+        texto2.innerHTML = "x"
+        texto2.style.border = "none"
         verificar = !verificar
     } else {
-        document.querySelector('.menu-toggle2').innerHTML = "Filtrar"
-        document.querySelector('.menu-toggle2').style.border = "1px solid"
+        texto2.innerHTML = "Filtrar"
+        texto2.style.border = "1px solid"
+        /**/
+        sectam.style.display = "none"
+        secCores.style.display = "none"
+        secPre.style.display = "none"
+        /**/
         verificar = true
     }
 })
+
+
+
+
+
+
+
+
+
+
+
+const secCores = document.querySelector('.secCores')
+var verifiCores = true
+document.querySelector('#plusCor').addEventListener("click", function(){
+    if(verifiCores){
+        secCores.style.display = "block"
+        verifiCores = !verifiCores
+    }else{
+        secCores.style.display = "none"
+        verifiCores = true
+    }
+})
+const sectam = document.querySelector('.secTamanhos')
+var verifitam = true
+document.querySelector('#plusTam').addEventListener("click", function(){
+    if(verifitam){
+        sectam.style.display = "grid"
+        secPre.style.marginTop = "150px" /*aberto */
+        verifitam = !verifitam
+    }else{
+        sectam.style.display = "none"
+        secPre.style.marginTop = "270px" /*fechado */
+        verifitam = true
+    }
+})
+const secPre = document.querySelector('.secprecos')
+var verifiPre = true
+document.querySelector('#plusPre').addEventListener("click", function(){
+    if(verifiPre){
+        secPre.style.display = "block"
+        verifiPre = !verifiPre
+    }else{
+        secPre.style.display = "none"
+        verifiPre = true
+    }
+})
+
