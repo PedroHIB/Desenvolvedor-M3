59e0f61ffbff711264d2efbaf9e2e03e69528825
Pedro HIB <pedrohizib@gmail.com>
2020-08-22 22:46:15 -0300

    ativando filtro de cores

--- js/produtos.js
+++ js/produtos.js
@@ -59,31 +59,146 @@
 
 
     /*Filtro de Cores */
-    $( "#amarelo" ).click(function() {
-        /*var filcor = [];
-        
-        products.forEach((filcor) => {
-            if(products.cores == "amarelo") {
-                filcor.push(products)
+    $( "#amarelo" ).click(function() { 
+        let filtroAmarelo = []  
+        for (var i = 0; i < 9; i++){
+            for (var j = 0; j < 3; j++){
+                if (products[i].cores[j] == 'amarelo'){
+                    filtroAmarelo.push(products[i])
+                }
             }
-        })
-        clear()
-        render(filcor) 
-        console.log(filcor)*/
-
-        /*products.forEach(function(el){
-            if(el.cores === "amarelo") {
-                alert ("achou!")
-            }
-        });*/
-
-        let re = []
-        products.filters(function(a) {
-            return re.push(a.cores == "Amarelo");
-        });
+        }
         
-        console.log(re)
         clear()
-        render(re) 
+        render(filtroAmarelo)
+    })
+    $( "#azul" ).click(function() {  
+        let filtroAzul = []         
+        for (var i = 0; i < 9; i++){
+            for (var j = 0; j < 3; j++){
+                if (products[i].cores[j] == 'azul'){
+                    filtroAzul.push(products[i])
+                }
+            }
+        }
+        
+        clear()
+        render(filtroAzul)
+    })
+    $( "#branco" ).click(function() { 
+        let filtroBranco = [] 
+        for (var i = 0; i < 9; i++){
+            for (var j = 0; j < 3; j++){
+                if (products[i].cores[j] == 'branco'){
+                    filtroBranco.push(products[i])
+                }
+            }
+        }        
+        clear()
+        render(filtroBranco)
+    })
+    $( "#cinza" ).click(function() { 
+        let filtroCinza = [] 
+        for (var i = 0; i < 9; i++){
+            for (var j = 0; j < 3; j++){
+                if (products[i].cores[j] == 'cinza'){
+                    filtroCinza.push(products[i])
+                }
+            }
+        }
+        
+        clear()
+        render(filtroCinza)
+    })
+    $( "#laranja" ).click(function() {  
+        let filtroLaranja = []  
+        for (var i = 0; i < 9; i++){
+            for (var j = 0; j < 3; j++){
+                if (products[i].cores[j] == 'laranja'){
+                    filtroLaranja.push(products[i])
+                }
+            }
+        }
+        
+        clear()
+        render(filtroLaranja)
+    })
+    $( "#laranja" ).click(function() {  
+        let filtroLaranja = []  
+        for (var i = 0; i < 9; i++){
+            for (var j = 0; j < 3; j++){
+                if (products[i].cores[j] == 'laranja'){
+                    filtroLaranja.push(products[i])
+                }
+            }
+        }
+        
+        clear()
+        render(filtroLaranja)
+    })
+    $( "#verde" ).click(function() {  
+        let filtroverde = []  
+        for (var i = 0; i < 9; i++){
+            for (var j = 0; j < 3; j++){
+                if (products[i].cores[j] == 'verde'){
+                    filtroverde.push(products[i])
+                }
+            }
+        }
+        
+        clear()
+        render(filtroverde)
+    })
+    $( "#vermelho" ).click(function() {  
+        let filtrovermelho = []  
+        for (var i = 0; i < 9; i++){
+            for (var j = 0; j < 3; j++){
+                if (products[i].cores[j] == 'vermelho'){
+                    filtrovermelho.push(products[i])
+                }
+            }
+        }
+        
+        clear()
+        render(filtrovermelho)
+    })
+    $( "#preto" ).click(function() {  
+        let filtropreto = []  
+        for (var i = 0; i < 9; i++){
+            for (var j = 0; j < 3; j++){
+                if (products[i].cores[j] == 'preto'){
+                    filtropreto.push(products[i])
+                }
+            }
+        }
+        
+        clear()
+        render(filtropreto)
+    })
+    $( "#rosa" ).click(function() {  
+        let filtrorosa = []  
+        for (var i = 0; i < 9; i++){
+            for (var j = 0; j < 3; j++){
+                if (products[i].cores[j] == 'rosa'){
+                    filtrorosa.push(products[i])
+                }
+            }
+        }
+        
+        clear()
+        render(filtrorosa)
+    })
+    $( "#vinho" ).click(function() {  
+        let filtrovinho = []  
+        for (var i = 0; i < 9; i++){
+            for (var j = 0; j < 3; j++){
+                if (products[i].cores[j] == 'vinho'){
+                    filtrovinho.push(products[i])
+                }
+            }
+        }
+        
+        clear()
+        render(filtrovinho)
     })
 })();
